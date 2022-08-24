0f5809564207feabb00439057fc328eeb6f27fb4
showjf <showjf2@gmail.com>
2022-08-24 21:54:24 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할일목록</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+//export default App;
