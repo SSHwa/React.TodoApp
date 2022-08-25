c5f29842fc7ef62cf0a415b74533cacd50149321
showjf <showjf2@gmail.com>
2022-08-25 20:41:31 +0900

    할일목록 작성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      TextDecoration: "none",
+    };
+  };
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+
+  handleXClick = (id) => {
+    let newTodoData = this.todoData.filter((x) => x.id !== id);
+    console.log("newTodoData", newTodoData);
+  };
+
   render() {
     return (
       <div className="container">
@@ -9,6 +44,19 @@ export default class App extends Component {
           <div className="title">
             <h1>할일목록</h1>
           </div>
+
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleXClick(data.id)}
+              >
+                x
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     );
